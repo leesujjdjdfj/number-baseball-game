3fae8e25240f78d73da78655999ef17ab1cbd201
leesujjdjdfj <dltjdgus1023@gmail.com>
2025-07-14 01:23:57 +0900

    Add files via upload

--- nbg_runner.py
+++ nbg_runner.py
@@ -1,6 +1,6 @@
-# nbg_runner.py
-
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("nbg_server:app_mount", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Render가 제공하는 PORT 사용
+    uvicorn.run("nbg_server:app_mount", host="0.0.0.0", port=port)
